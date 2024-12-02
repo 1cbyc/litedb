2a0f9b543b4ed111e91ecd2ac56ba7d6c14bc48a
1cbyc <isaacnsisong@gmail.com>
2024-12-02 13:42:20 +0100

    trying to build a dbms i will use

---
+++ include/litedb.h
@@ -0,0 +1,14 @@
+#ifndef LITE_DB_H
+#define LITE_DB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_RECORD_SIZE 256
+
+// Function prototypes
+void write_record(const char *filename, const char *record);
+void read_records(const char *filename);
+
+#endif // LITE_DB_H
